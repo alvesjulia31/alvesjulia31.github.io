131a6eb52fd50b46c8875583b316ad8d38f99466
nirvanamach <andre.machado93@gmail.com>
2026-05-21 22:43:48 +0100

    clean

--- vite.config.js
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  // Ajusta o base path para o nome do teu repositório no GitHub Pages
-  // base: '/o-teu-repositorio/', 
-})
